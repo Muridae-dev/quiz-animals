1b651e55513e113ab5ffb1274baad40bd1ee05dc
LinaSimon <90833662+LinaSimon@users.noreply.github.com>
2021-11-10 15:26:05 +0100

    linas changes

--- main/sabimango.js
+++ main/sabimango.js
@@ -3,42 +3,43 @@ var labelBox;
 // k = every radiobutton is called k1, k2, k3, k4
 var k4;
 var k5;
+
+// ARRAY OF OBJECTS
 var quiz = [
   {
-    fraga:
-      "Name the artist!<br/>",
+    fraga: "Name the artist!<br/>",
     img: "<img src='images/verka-serduchka.gif'>",
-    svar: [
-      "Jerka Babuchka",
-      "Verka Serduchka",
-      "Vixen Verkuchka",
-    ],
+    svar: ["Jerka Babuchka", "Verka Serduchka", "Vixen Verkuchka"],
     answer: "k2",
     nrQ: 3,
   },
   {
     fraga: "Which is the best designer?",
-    svar: ["Prada", "Gucci", "Chanel"],
+    img: "<img src='images/fashion.png'>",
+    svar: ["Prada", "Gucci", "Louis Vuitton"],
     answer: "k1",
     nrQ: 3,
   },
   {
     fraga: "Which is the best company?",
+    img: "<img src='images/games.png'>",
     svar: ["Sony", "Nintendo", "Microsoft"],
     answer: "k2",
     nrQ: 3,
   },
   {
     fraga: "Which is the best snacks?",
+    img: "<img id='sharkImage' src='images/sharkfood.png'>",
     svar: ["Doritos", "Pringles", "Lays", "Estrella", "OLW"],
     answer: "k4",
     nrQ: 5,
   },
 ];
-
+// ADDING THE CONTENT (IMGDIV, RADIOBOXES, LABELBOXES, ALL EMPTY) 
 function initSabimango() {
-  var content = '      <div id="question"><div id="imgBox"></div>   <label for="k1"></label>      <input type="radio" id="k1" name="fraga1" /> <br />      <label for="k2"></label>      <input type="radio" id="k2" name="fraga1" /> <br />      <label for="k3"></label>      <input type="radio" id="k3" name="fraga1" /> <br />      <label for="k4"></label>      <input type="radio" id="k4" name="fraga1" /> <br />      <label for="k5"></label>      <input type="radio" id="k5" name="fraga1" /> <br />      <button id="submit" onclick="checkMangoAnswer()">Show results</button>    </div> </div>  ';
+  var content = '      <div id="question"><div id="imgBox"></div>   <label for="k1"></label>      <input type="radio" id="k1" name="fraga1" /> <br />      <label for="k2"></label>      <input type="radio" id="k2" name="fraga1" /> <br />      <label for="k3"></label>      <input type="radio" id="k3" name="fraga1" /> <br />      <label for="k4"></label>      <input type="radio" id="k4" name="fraga1" /> <br />      <label for="k5"></label>      <input type="radio" id="k5" name="fraga1" /> <br />      <button id="submit" onclick="checkMangoAnswer()">Submit!</button>    </div> </div>  ';
   containerBox.innerHTML = content;
+  // IF THE QUESTION HAS 5 ALTERNATIVES, TO CHANGE HOW MANY RADIOBUTTONS WE DISPLAY
   k5 = document.getElementById("k5");
   k4 = document.getElementById("k4");
   labelBox = document.getElementsByTagName("label");
@@ -54,7 +55,7 @@ function checkMangoAnswer() {
   } else {
     wrongAnswer();
   }
-  /*  SETTIMEOUT FOR CHANGE QUESTION WITH EX 3000MS
+  /*  SET TIMEOUT FOR CHANGE QUESTION WITH EX 3000MS
   MAKE DISPLAY INLINE FOR THE CONTAINER DIV IN CHANGEQUESTION. THEN HIDE THE RESULT BOX IN THE SAME WAY */
   quiz.splice(randomPicker, 1);
 }
@@ -65,7 +66,6 @@ function checkMangoAnswer() {
 function changeQuestion() {
   //MAKE THE CONTAINER DIV DISPLAY INLINE
   //containerBox.style.display = "";
-
   randomPicker = random(quiz);
   textBox.innerHTML =  quiz[randomPicker].fraga;
   if(quiz[randomPicker].img) document.getElementById("imgBox").innerHTML = quiz[randomPicker].img;
